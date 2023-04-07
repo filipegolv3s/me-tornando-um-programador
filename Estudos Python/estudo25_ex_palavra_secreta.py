c227b49e37defb2d798c2f56ab578b53462a9ca8
filipe-golves-dev <filipegolves@gmail.com>
2023-04-06 22:36:01 -0300

    Exercicio finalizado!

--- Estudos Python/estudo25_ex_palavra_secreta.py
+++ Estudos Python/estudo25_ex_palavra_secreta.py
@@ -1,10 +1,41 @@
+import os
 palavra_secreta = 'Interstellar'
-palavra_completa = ''
-#palavra_oculta = ''
+letras_acertadas = ''
+numero_tentativas = 0
 
-# For passa em cada silaba da palavra secreta, concatenando e tornando uma só
-for silaba_separada in palavra_secreta:
-    silaba_separada = '*'
-    palavra_completa += silaba_separada
+while True:
+    letra_chutada = input('Digite uma letra: ')
+    numero_tentativas += 1
 
-silaba_chutada = ...
+    #Se tudo ocorrer bem, essa função abaixo será ignorada, se o usuário digitar mais de uma letra, ela será executada.
+    if len(letra_chutada) > 1:
+        print('Digite apenas uma letra!')
+        continue
+
+    #1- Descobrir se a letra chutada está na palavra secreta:
+    if letra_chutada in palavra_secreta:
+        letras_acertadas += letra_chutada
+
+    #3
+    palavra_completa = ''
+
+    #for faz um loop da função toda, mas pra cada letra. Ou seja, ele começa na primeira, ver se o if vai rodar, se não rodar, vai para o else. #2
+    for letra_apagada in palavra_secreta:
+        if letra_apagada in letras_acertadas:
+            palavra_completa += letra_apagada
+        else:
+            palavra_completa += '*'
+
+    print('Palavra formada: {0}'.format(palavra_completa))
+
+    if palavra_completa == palavra_secreta:
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+        print('Parabéns! Você acertou.')
+        print('A palavra era: {}'.format(palavra_secreta))
+        print('Tentativas: {}'.format(numero_tentativas))
+        #reiniciar o codigo
+        letras_acertadas = ''
+        numero_tentativas = 0
