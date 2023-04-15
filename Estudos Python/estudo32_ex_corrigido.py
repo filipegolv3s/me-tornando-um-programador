8b1a4340a58195e2a18a3e741b257e5c45466732
filipe-golves-dev <filipegolves@gmail.com>
2023-04-15 01:32:45 -0300

    Estudo #12

--- Estudos Python/estudo32_ex_corrigido.py
+++ Estudos Python/estudo32_ex_corrigido.py
@@ -1,58 +1,43 @@
-cpf = '69369731034'
-nove_digitos = 0
-contador_regressivo = []
-i_contador = 0
+import re
+import sys
+
+cpf = re.sub(
+    r'[^0-9]',
+    '',
+    input('Digite seu cpf(sem pontos): ')
+)
+caracteres_repitidos = cpf == cpf[0] * len(cpf)
+# cpf = input('Digite seu cpf(sem pontos): ').replace('.', '').replace('-', '').replace(' ', '')
+if caracteres_repitidos:
+    print('Você digitou dados repetidos.')
+
+
+nove_digitos = cpf[:9]
+contador_regressivo = 10
 
 resultado = 0
+for digito in nove_digitos:
+    resultado += int(digito) * contador_regressivo
+    contador_regressivo -= 1
 
-#cpf = input('Digite seu cpf (somente com números): ')
-nove_digitos = cpf[:9]
-if cpf.isdigit():
-    for contador in range(10, 0, -1):
-        contador_regressivo.append(contador)
-        
-
-    for digito in nove_digitos:
-        resultado += int(digito) * contador_regressivo[i_contador]
-        i_contador += 1
-
-    digito_1 = (resultado * 10) % 11
-
-    # SEGUNDO TESTE:
-
-    if digito_1 <= 9:
-        print( 'Primeiro teste, passou!')
-        dez_digitos = 0
-        contador_regressivo_2 = []
-        i_contador_2 = 0
-
-        resultado_2 = 0
-
-        dez_digitos = cpf[:10]
-
-        for contador_2 in range(11, 0, -1):
-            contador_regressivo_2.append(contador_2)
-        
-        print(contador_regressivo_2)
-
-        
-        for digito_2 in dez_digitos:
-            resultado_2 += int(digito_2) * contador_regressivo_2[i_contador_2]
-            print(f'A soma de {int(digito_2)} x {contador_regressivo_2[i_contador_2]} é {resultado_2}')
-            i_contador_2 += 1
-
-        digito_2 = (resultado_2 * 10) % 11
+tratando_digito_1 = (resultado * 10) % 11
+digito_1 = tratando_digito_1 if tratando_digito_1 <= 9 else 0
 
 
-        if digito_2 > 9:
-            digito_2 = 0
-            print('Cpf inválido')
-        else:
-            'Digito Válido!'
+dez_digitos = cpf[:9] + str(tratando_digito_1)
+segundo_contador_regressivo = 11
 
+segundo_resultado = 0
+for digito in dez_digitos:
+    segundo_resultado += int(digito) * segundo_contador_regressivo
+    segundo_contador_regressivo -= 1
 
-    else:
-        print('Cpf falso!')
+tratando_digito_2 = (segundo_resultado * 10) % 11
+digito_2 = tratando_digito_2 if tratando_digito_2 <= 9 else 0
 
+cpf_recriado = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_recriado == cpf:
+    print('{c1} é um cpf válido!'.format(c1 = cpf))
 else:
-    print('Digite apenas números!: ')
+    print('{c1} é um cpf inválido!'.format(c1 = cpf))
